792835d2475ada47897212172f6028c56e308a8b
Enieber Cunha <eniebercunha@gmail.com>
2024-11-18 19:06:37 -0300

    feat: import post with html tranforming in lexical

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,176 @@ use axum::{
 use dotenv::dotenv;
 use mysql::prelude::Queryable;
 use mysql::Pool;
+use scraper::{Html, Selector};
 use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 use std::env;
 use tokio::net::TcpListener;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use uuid::Uuid;
+
+fn generate_truncated_uuid() -> String {
+    let uuid = Uuid::new_v4(); // Gera um UUID v4 aleatório
+    let hex = uuid.as_simple().to_string(); // Formato sem hífens
+    hex[..24].to_string() // Trunca para 24 caracteres
+}
+
+#[derive(Deserialize, Serialize)]
+struct Tag {
+    id: Option<u64>, // Opcional porque o ID será gerado automaticamente
+    name: String,
+    slug: String,
+}
+
+fn html_to_mobiledoc(html: &str) -> Value {
+    let document = Html::parse_document(html);
+    let p_selector = Selector::parse("p").unwrap();
+
+    let mut children_blocks = vec![];
+
+    for p in document.select(&p_selector) {
+        let mut children_text = vec![];
+
+        for text_node in p.text() {
+            children_text.push(json!({
+                "detail": 0,
+                "format": 0,
+                "mode": "normal",
+                "style": "",
+                "text": text_node,
+                "type": "extended-text",
+                "version": 1
+            }));
+        }
+
+        children_blocks.push(json!({
+            "children": children_text,
+            "direction": "ltr",
+            "format": "",
+            "indent": 0,
+            "type": "paragraph",
+            "version": 1
+        }));
+    }
+
+    // Retorna a estrutura final
+    json!({
+        "root": {
+            "children": children_blocks,
+            "direction": "ltr",
+            "format": "",
+            "indent": 0,
+            "type": "root",
+            "version": 1
+        }
+    })
+}
+
+async fn add_tag(Json(tag): Json<Tag>) -> impl IntoResponse {
+    let db_url = env::var("DB_URL").unwrap();
+    let connection_opts = mysql::Opts::from_url(&db_url).unwrap();
+    let pool = Pool::new(connection_opts).unwrap();
+    let mut conn = pool.get_conn().unwrap();
+
+    let result = conn.exec_drop(
+        "INSERT INTO tags (name, slug, created_at, updated_at, created_by) VALUES (?, ?, NOW(), NOW(), 1)",
+        (&tag.name, &tag.slug),
+    );
+
+    match result {
+        Ok(_) => {
+            tracing::trace!("add_tag succeeded in inserting new tag");
+            // Obtém o ID da tag recém-criada
+            let tag_id = conn.last_insert_id();
+
+            let response = Tag {
+                id: Some(tag_id),
+                name: tag.name,
+                slug: tag.slug,
+            };
+            (StatusCode::CREATED, Json(response)).into_response()
+        }
+        Err(e) => {
+            tracing::error!("add_tag failed to insert new tag: {:?}", &e);
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to create tag: {}", e),
+            )
+                .into_response()
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+struct Post {
+    id: Option<u64>, // Opcional porque o ID será gerado automaticamente
+    title: String,
+    slug: String,
+    html: String,
+    created_at: String,
+    updated_at: String,
+    author_id: u64,
+}
+
+#[derive(Deserialize, Serialize)]
+struct PostReply {
+    id: Option<u64>, // Opcional porque o ID será gerado automaticamente
+    title: String,
+    slug: String,
+    created_at: String,
+    updated_at: String,
+    author_id: u64,
+}
+
+async fn add_post(Json(post): Json<Post>) -> impl IntoResponse {
+    let db_url = env::var("DB_URL").unwrap();
+    let connection_opts = mysql::Opts::from_url(&db_url).unwrap();
+    let pool = Pool::new(connection_opts).unwrap();
+    let mut conn = pool.get_conn().unwrap();
+    let post_id = generate_truncated_uuid();
+    let uuid = Uuid::new_v4().to_string();
+    let content = html_to_mobiledoc(&post.html);
+    let result = conn.exec_drop(
+        "INSERT INTO posts (id, uuid, title, slug, html, lexical, created_at, updated_at, created_by, email_recipient_filter) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, 'all')",
+        (
+            &post_id,
+            &uuid,
+            &post.title,
+            &post.slug,
+            &post.html,
+            &content,
+            &post.created_at,
+            &post.updated_at,
+            &post.author_id,
+        ),
+    );
+
+    match result {
+        Ok(_) => {
+            tracing::trace!("add_post succeeded in inserting new post");
+            // Obtém o ID do post recém-criado
+            let post_id = conn.last_insert_id();
+
+            let response = PostReply {
+                id: Some(post_id),
+                title: post.title,
+                slug: post.slug,
+                created_at: post.created_at,
+                updated_at: post.updated_at,
+                author_id: post.author_id,
+            };
+            (StatusCode::CREATED, Json(response)).into_response()
+        }
+        Err(e) => {
+            tracing::error!("add_post failed to insert new post: {:?}", &e);
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to create post: {}", e),
+            )
+                .into_response()
+        }
+    }
+}
 
 #[derive(Deserialize, Serialize)]
 struct User {
@@ -27,6 +193,7 @@ struct User {
 
 #[derive(Deserialize, Serialize)]
 struct Author {
+    id: u64,
     name: String,
     email: String,
 }
@@ -49,6 +216,7 @@ async fn add_author(Json(user): Json<User>) -> impl IntoResponse {
             let author_id = conn.last_insert_id();
 
             let response = Author {
+                id: author_id,
                 name: user.name,
                 email: user.email,
             };
@@ -87,6 +255,8 @@ async fn main() {
     let app = Router::new()
         .route("/api/healthcheck", get(health_check_handler))
         .route("/api/authors", post(add_author))
+        .route("/api/tags", post(add_tag))
+        .route("/api/posts", post(add_post))
         .layer(middleware::from_fn(validation_fingerprint));
 
     println!("🚀 Server started");
